74c2e4932efc2e54a2396dfe3efa1ce5559ed55a
Qingliao Wu <qingliaowu@gmail.com>
2020-08-02 12:22:07 +0900

    update

---
+++ JavaCode/src/main/java/javabasics/HelloWorld.java
@@ -0,0 +1,9 @@
+package javabasics;
+
+// A Java program to print "Hello World"
+public class HelloWorld {
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+    }
+}
