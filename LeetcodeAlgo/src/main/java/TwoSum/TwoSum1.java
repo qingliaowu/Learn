710b20578c89be42196ebb5ef5c30d79bbdff0a1
Qingliao Wu <qingliaowu@gmail.com>
2020-08-03 21:20:42 +0900

    update

--- LeetcodeAlgo/src/main/java/TwoSum/TwoSum1.java
+++ LeetcodeAlgo/src/main/java/TwoSum/TwoSum1.java
@@ -7,16 +7,27 @@ import java.util.Arrays;
 public class TwoSum1 {
 
     public static void main(String[] args) {
-        int[] data = {2,5,5,6};
-        int[] test = new TwoSum1().twoSum(data,10);
+        //index 0,1,2,3
+        int[] nums = {3,5,5,6};
+        int[] test = new TwoSum1().twoSum(nums,11);
         System.out.println(Arrays.toString(test));
     }
 
+    //Time complexity O(n^2), which is actually bad.
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) { // Time complexity O(n^2)
+        //nums.length = 4
+        for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
+                //5 != 11 - 3 = 8    i=0, j=1
+                //5 != 11 - 3 = 8    i=0, j=2
+                //6 != 11 - 3 = 8    i=0, j=3
+
+                //5 != 11 - 5 = 6    i=1,j=2
+                //6 == 11 - 5      i=1, j=3
                 if (nums[j] == target - nums[i]) {
+                    //return 1,3
                     return new int[] { i, j };
+
                 }
             }
         }
