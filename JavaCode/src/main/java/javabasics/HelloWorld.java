02b7193e60aaa40aabd440e3c2a589cc6fc26bad
David Wu <qingliaowu@gmail.com>
2020-08-10 01:24:51 +0900

    update

--- JavaCode/src/main/java/javabasics/HelloWorld.java
+++ JavaCode/src/main/java/javabasics/HelloWorld.java
@@ -2,6 +2,8 @@ package javabasics;
 
 // A Java program to print "Hello World"
 public class HelloWorld {
+
+    //main class is the entry point of a program
     public static void main(String args[])
     {
         System.out.println("Hello World");
