19399fc772ce9ee3780aaf14ba3a189eb69f510e
Qingliao Wu <qingliaowu@gmail.com>
2020-09-12 23:44:39 +0900

    update

--- JavaCode/src/main/java/javabasics/SavingsAccount.java
+++ JavaCode/src/main/java/javabasics/SavingsAccount.java
@@ -8,6 +8,20 @@ public class SavingsAccount {
         balance = initialBalance;
     }
 
+    public void checkBalance(){
+        System.out.println("Hello!");
+        System.out.println("Your balance is "+ balance);
+    }
+
+    public void deposit(int amountToDeposit){
+        this.balance = balance + amountToDeposit;
+    }
+
+    public int withdraw (int amountToWithdraw){
+        this.balance = balance - amountToWithdraw;
+        return amountToWithdraw;
+    }
+
     public static void main(String[] args){
         SavingsAccount savings = new SavingsAccount(2000);
 
@@ -16,27 +30,32 @@ public class SavingsAccount {
         System.out.println("Your balance is "+savings.balance);
 
         //Withdrawing:
-        int afterWithdraw = savings.balance - 300;
-        savings.balance = afterWithdraw;
-        System.out.println("You just withdrew "+300);
+        savings.withdraw(300);
+        //int afterWithdraw = savings.balance - 300;
+        savings.balance = savings.withdraw(300);
+
+        System.out.println("You just withdrew "+ savings.withdraw(300));
 
         //Check balance:
-        System.out.println("Hello!");
-        System.out.println("Your balance is "+savings.balance);
-
+        // System.out.println("Hello!");
+        // System.out.println("Your balance is "+savings.balance);
+        savings.checkBalance();
         //Deposit:
         int afterDeposit = savings.balance + 600;
         savings.balance = afterDeposit;
         System.out.println("You just deposited "+600);
 
         //Check balance:
-        System.out.println("Hello!");
-        System.out.println("Your balance is "+savings.balance);
-
+        // System.out.println("Hello!");
+        // System.out.println("Your balance is "+savings.balance);
+        savings.checkBalance();
         //Deposit:
-        int afterDeposit2 = savings.balance + 600;
-        savings.balance = afterDeposit2;
-        System.out.println("You just deposited "+600);
+        // int afterDeposit2 = savings.balance + 600;
+        // savings.balance = afterDeposit2;
+        // System.out.println("You just deposited "+600);
+
+        savings.deposit(600);
+
 
         //Check balance:
         System.out.println("Hello!");
