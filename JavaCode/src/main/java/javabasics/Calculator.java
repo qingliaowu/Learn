faa26c482032b5aefd06c355b418ed59e0b4bd60
Qingliao Wu <qingliaowu@gmail.com>
2020-09-12 23:57:35 +0900

    update

--- JavaCode/src/main/java/javabasics/Calculator.java
+++ JavaCode/src/main/java/javabasics/Calculator.java
@@ -8,4 +8,30 @@ public class Calculator {
     public int add(int a, int b){
         return a + b;
     }
+
+    public int subtract(int a, int b){
+        return a - b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public int divide(int a, int b){
+        return a / b;
+    }
+
+    public int modulo(int a, int b){
+        return a % b;
+    }
+
+    public static void main(String args[]){
+
+        Calculator myCalculator = new Calculator();
+
+        System
+
+
+    }
+
 }
