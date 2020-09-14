3b7ae77425c24d6fd383899e39ced53a652d4130
Qingliao Wu <qingliaowu@gmail.com>
2020-09-14 17:13:18 +0900

    update

--- JavaCode/src/main/java/javabasics/Order.java
+++ JavaCode/src/main/java/javabasics/Order.java
@@ -1,14 +1,39 @@
 package javabasics;
 
 public class Order {
+    boolean isFilled;
+    double billAmount;
+    String shipping;
 
-    public static void main(String[] args) {
+    public Order(boolean filled, double cost, String shippingMethod) {
+        if (cost > 24.00) {
+            System.out.println("High value item!");
+        }
+        isFilled = filled;
+        billAmount = cost;
+        shipping = shippingMethod;
+    }
 
-        double itemCost = 30.99;
+    public void ship() {
+        if (isFilled) {
+            System.out.println("Shipping");
+            System.out.println("Shipping cost: " + calculateShipping());
+        } else {
+            System.out.println("Order not ready");
+        }
+    }
 
-        // Write an if-then statement:
-
+    public double calculateShipping() {
+        // declare conditional statement here
 
     }
 
+    public static void main(String[] args) {
+        // do not alter the main method!
+        Order book = new Order(true, 9.99, "Express");
+        Order chemistrySet = new Order(false, 72.50, "Regular");
+
+        book.ship();
+        chemistrySet.ship();
+    }
 }
