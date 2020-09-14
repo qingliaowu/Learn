b964ff803f24ce01a8ac6a0c668ffba4eedd4fba
Qingliao Wu <qingliaowu@gmail.com>
2020-09-14 16:04:36 +0900

    update

---
+++ JavaCode/src/main/java/javabasics/Order.java
@@ -0,0 +1,14 @@
+package javabasics;
+
+public class Order {
+
+    public static void main(String[] args) {
+
+        double itemCost = 30.99;
+
+        // Write an if-then statement:
+
+
+    }
+
+}
