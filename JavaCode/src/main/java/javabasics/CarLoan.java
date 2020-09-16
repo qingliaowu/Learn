fb0b89c9451659215346cb789140fc83e3b3ca9e
Qingliao Wu <qingliaowu@gmail.com>
2020-09-17 07:25:31 +0900

    update

--- JavaCode/src/main/java/javabasics/CarLoan.java
+++ JavaCode/src/main/java/javabasics/CarLoan.java
@@ -1,4 +1,25 @@
 package javabasics;
 
 public class CarLoan {
+    public static int carLoan = 10000;
+    public static int loanLength = 3; //3 years
+    public static int interestRate = 5; //an interest rate of 5% on the loan
+    static int downPayment = 2000;
+
+
+    public static void main(String[] args) {
+
+        if(loanLength <= 0){
+            System.out.println("Error! You must take out a valid car loan.");
+        }else if(downPayment >= carLoan){
+            System.out.println("The car can be paid in full.");
+        }else{
+            int remainingBalance = carLoan - downPayment;
+            int months = loanLength * 12;
+            int monthlyBalance = remainingBalance / months;
+            int interest = monthlyBalance * interestRate / 100;
+            int monthlyPayment = monthlyBalance + interest;
+            System.out.println(monthlyPayment);
+        }
+    }
 }
