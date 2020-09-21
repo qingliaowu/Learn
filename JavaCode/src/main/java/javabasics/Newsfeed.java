1dab9196cdbeffc191e8da006c397c170eb7aa46
Qingliao Wu <qingliaowu@gmail.com>
2020-09-21 19:44:46 +0900

    update

--- JavaCode/src/main/java/javabasics/Newsfeed.java
+++ JavaCode/src/main/java/javabasics/Newsfeed.java
@@ -1,25 +1,40 @@
 package javabasics;
 
-// import the Arrays package here:
 import java.util.Arrays;
 
 public class Newsfeed {
 
+    String[] topics = {"Opinion", "Tech", "Science", "Health"};
+    int[] views = {0, 0, 0, 0};
 
     public Newsfeed(){
 
     }
 
     public String[] getTopics(){
-        String[] topics = {"Opinion", "Tech", "Science", "Health"};
         return topics;
     }
 
+    public String getTopTopic(){
+
+    }
+
+    public void viewTopic(int topicIndex){
+
+    }
 
     public static void main(String[] args){
         Newsfeed sampleFeed = new Newsfeed();
-        String[] topics = sampleFeed.getTopics();
-        System.out.println(topics);
-        System.out.println(Arrays.toString(topics));
+
+        System.out.println("The top topic is "+ sampleFeed.getTopTopic());
+
+        sampleFeed.viewTopic(1);
+        sampleFeed.viewTopic(1);
+        sampleFeed.viewTopic(3);
+        sampleFeed.viewTopic(2);
+        sampleFeed.viewTopic(2);
+        sampleFeed.viewTopic(1);
+
+        System.out.println("The " + sampleFeed.topics[1] + " topic has been viewed " + sampleFeed.views[1] + " times!");
     }
 }
