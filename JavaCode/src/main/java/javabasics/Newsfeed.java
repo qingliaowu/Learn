3e38d3701b0ef801ea0aff85d8241c86b7e360d7
Qingliao Wu <qingliaowu@gmail.com>
2020-09-23 19:43:10 +0900

    update

--- JavaCode/src/main/java/javabasics/Newsfeed.java
+++ JavaCode/src/main/java/javabasics/Newsfeed.java
@@ -6,36 +6,30 @@ public class Newsfeed {
 
     String[] topics = {"Opinion", "Tech", "Science", "Health"};
     int[] views = {0, 0, 0, 0};
+    String[] favoriteArticles;
 
     public Newsfeed(){
+        // Initialize favoriteArticles here:
+        favoriteArticles = new String[10];
+
 
     }
 
-    public String[] getTopics(){
-        return topics;
-    }
+    public void setFavoriteArticle(int favoriteIndex, String newArticle){
+        // Add newArticle to favoriteArticles:
+        favoriteArticles[favoriteIndex]
+                = newArticle;
 
-    public String getTopTopic(){
-        return topics[0];
-    }
-
-    public void viewTopic(int topicIndex){
-        views[topicIndex] = views[topicIndex] + 1;
     }
 
     public static void main(String[] args){
         Newsfeed sampleFeed = new Newsfeed();
 
-        System.out.println("The top topic is "+ sampleFeed.getTopTopic());
 
-        sampleFeed.viewTopic(1);
-        sampleFeed.viewTopic(1);
-        sampleFeed.viewTopic(3);
-        sampleFeed.viewTopic(2);
-        sampleFeed.viewTopic(2);
-        sampleFeed.viewTopic(1);
+        sampleFeed.setFavoriteArticle(2, "Humans: Exterminate Or Not?");
+        sampleFeed.setFavoriteArticle(3, "Organic Eye Implants");
+        sampleFeed.setFavoriteArticle(0, "Oil News");
 
-        System.out.println("The " + sampleFeed.topics[1] + " topic has been viewed " + sampleFeed.views[1] + " times!");
-
+        System.out.println(Arrays.toString(sampleFeed.favoriteArticles));
     }
 }
