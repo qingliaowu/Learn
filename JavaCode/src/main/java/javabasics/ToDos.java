b3f1c0da9241070a7cc4e94f9f514e7a91686e77
Qingliao Wu <qingliaowu@gmail.com>
2020-09-27 00:50:51 +0900

    update

---
+++ JavaCode/src/main/java/javabasics/ToDos.java
@@ -0,0 +1,22 @@
+package javabasics;
+
+import java.util.ArrayList;
+
+class ToDos {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> toDoList = new ArrayList<String>();
+        String toDo1 = "Water plants";
+        // Add more to-dos here:
+
+
+        // Add to-dos to toDoList
+
+
+        System.out.println(toDoList);
+
+
+    }
+
+}
