864268ddbd326f44f478b8baedef005875f016d6
Qingliao Wu <qingliaowu@gmail.com>
2020-09-27 21:48:33 +0900

    update

--- JavaCode/src/main/java/javabasics/ToDos.java
+++ JavaCode/src/main/java/javabasics/ToDos.java
@@ -9,10 +9,15 @@ class ToDos {
         ArrayList<String> toDoList = new ArrayList<String>();
         String toDo1 = "Water plants";
         // Add more to-dos here:
+        String toDo2 = "task2";
+        String toDo3 = "Task3";
 
 
         // Add to-dos to toDoList
 
+        toDoList.add(toDo1);
+        toDoList.add(toDo2);
+        toDoList.add(toDo3);
 
         System.out.println(toDoList);
 
