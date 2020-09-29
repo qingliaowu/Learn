d4c78b92b485a4efd4ac217dcfb4394fc9b97aab
Qingliao Wu <qingliaowu@gmail.com>
2020-09-29 15:37:12 +0900

    update

--- JavaCode/src/main/java/javabasics/ToDos.java
+++ JavaCode/src/main/java/javabasics/ToDos.java
@@ -12,26 +12,19 @@ class ToDos {
         sherlocksToDos.add("visit the crime scene");
         sherlocksToDos.add("play violin");
         sherlocksToDos.add("interview suspects");
+        sherlocksToDos.add("listen to Dr. Watson for amusement");
         sherlocksToDos.add("solve the case");
         sherlocksToDos.add("apprehend the criminal");
 
-        // Poirot
-        ArrayList<String> poirotsToDos = new ArrayList<String>();
-
-        poirotsToDos.add("visit the crime scene");
-        poirotsToDos.add("interview suspects");
-        poirotsToDos.add("let the little grey cells do their work");
-        poirotsToDos.add("trim mustache");
-        poirotsToDos.add("call all suspects together");
-        poirotsToDos.add("reveal the truth of the crime");
-
-        // Remove each to-do below:
-        poirotsToDos.remove(0);
         sherlocksToDos.remove("visit the crime scene");
-        sherlocksToDos.remove("play violin");
 
-        System.out.println(sherlocksToDos.toString() + "\n");
-        System.out.println(poirotsToDos.toString());
+        // Calculate to-dos until case is solved:
+
+
+
+        // Change the value printed:
+        System.out.println("PRINT THE ANSWER HERE");
+
     }
 
 }
