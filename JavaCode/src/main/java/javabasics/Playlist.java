5ac06e8c2c25dc37974bcd19f7af9f476cce172e
Qingliao Wu <qingliaowu@gmail.com>
2020-09-29 22:03:54 +0900

    update

--- JavaCode/src/main/java/javabasics/Playlist.java
+++ JavaCode/src/main/java/javabasics/Playlist.java
@@ -5,9 +5,16 @@ import java.util.ArrayList;
 class Playlist {
 
 
-
-
     public static void main(String[] args) {
+
+        ArrayList<String> desertIslandPlaylist = new ArrayList<>();
+        desertIslandPlaylist.add("Song1");
+        desertIslandPlaylist.add("Song2");
+        desertIslandPlaylist.add("Song3");
+        desertIslandPlaylist.add("Song4");
+        desertIslandPlaylist.add("Song5");
+
+        System.out.println(desertIslandPlaylist.get(1));
 
     }
 
