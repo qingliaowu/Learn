4b550f8b5d0dcf91ca022a954e73131124fd4531
Qingliao Wu <qingliaowu@gmail.com>
2020-10-01 15:49:50 +0900

    update

---
+++ JavaCode/src/main/java/javabasics/CalculateTotal.java
@@ -0,0 +1,24 @@
+package javabasics;
+
+import java.util.ArrayList;
+
+class CalculateTotal {
+
+    public static void main(String[] args) {
+
+        ArrayList<Double> expenses = new ArrayList<Double>();
+        expenses.add(74.46);
+        expenses.add(63.99);
+        expenses.add(10.57);
+        expenses.add(81.37);
+
+        double total = 0;
+
+        // Iterate over expenses
+
+
+        System.out.println(total);
+
+    }
+
+}
