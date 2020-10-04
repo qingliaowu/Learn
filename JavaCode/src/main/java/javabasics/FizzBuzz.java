a344e5218e32fde2a1f293e4f3d51105f3116598
Qingliao Wu <qingliaowu@gmail.com>
2020-10-04 18:49:16 +0900

    update

--- JavaCode/src/main/java/javabasics/FizzBuzz.java
+++ JavaCode/src/main/java/javabasics/FizzBuzz.java
@@ -1,4 +1,14 @@
 package javabasics;
 
-public class FizzBuzz {
+public class FizzBuzz{
+
+    public static void main(String[] args){
+
+        for(int i=1;i<=100;i++){
+
+            System.out.println(i);
+        }
+
+    }
+
 }
