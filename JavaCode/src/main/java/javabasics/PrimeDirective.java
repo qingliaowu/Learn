b6d7d1fa04e294f0bffc790a115dca392de73875
Qingliao Wu <qingliaowu@gmail.com>
2020-10-07 13:48:41 +0900

    update

--- JavaCode/src/main/java/javabasics/PrimeDirective.java
+++ JavaCode/src/main/java/javabasics/PrimeDirective.java
@@ -1,4 +1,17 @@
 package javabasics;
 
-public class PrimeDirective {
+// Import statement:
+
+class PrimeDirective {
+
+    // Add your methods here:
+
+
+    public static void main(String[] args) {
+
+        PrimeDirective pd = new PrimeDirective();
+        int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
+
+    }
+
 }
