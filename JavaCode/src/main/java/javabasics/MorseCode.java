88ae731dee58af2c1326c20debebd1986e3f18e8
Qingliao Wu <qingliaowu@gmail.com>
2020-10-24 11:09:14 +0900

    update

---
+++ JavaCode/src/main/java/javabasics/MorseCode.java
@@ -0,0 +1,14 @@
+package javabasics;
+
+public class MorseCode {
+
+    public static void main(String[] args) {
+
+        String code = ".... .. / .. .----. -- / ... --- -. -. -.-- / .- -. -.. / .. / .-.. .. -.- . / - --- / . .- - / .-. .- -- . -.";
+
+        // Write the code:
+
+
+    }
+
+}
