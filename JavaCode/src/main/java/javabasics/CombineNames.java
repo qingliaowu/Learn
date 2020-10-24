aed0a6e3c59599259340486681f6bc1ba7fa413a
Qingliao Wu <qingliaowu@gmail.com>
2020-10-24 09:59:15 +0900

    update

---
+++ JavaCode/src/main/java/javabasics/CombineNames.java
@@ -0,0 +1,16 @@
+package javabasics;
+
+public class CombineNames {
+
+    public static void main(String[] args) {
+
+        // Add your first names:
+        String firstName = "ff";
+        String lastName = "sdfds";
+
+        // What's the full name?
+        System.out.println(firstName.concat(lastName));
+
+    }
+
+}
