32e739da8d2b72be25ad493e9d60467ba82e1adb
Qingliao Wu <qingliaowu@gmail.com>
2020-10-24 09:44:04 +0900

    update

---
+++ JavaCode/src/main/java/javabasics/TweetCounter.java
@@ -0,0 +1,15 @@
+package javabasics;
+
+public class TweetCounter {
+
+    public static void main(String[] args) {
+
+        String tweet = "Liz Lemon, ninjas are kind of cool... I just dont know any personally. Get on that.";
+
+        // What's the character count?
+        System.out.println(tweet.length());
+
+
+    }
+
+}
