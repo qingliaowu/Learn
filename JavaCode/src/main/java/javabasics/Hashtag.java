9232fe26ae0229663d87870e20a693e848daa855
Qingliao Wu <qingliaowu@gmail.com>
2020-10-24 12:23:16 +0900

    update

---
+++ JavaCode/src/main/java/javabasics/Hashtag.java
@@ -0,0 +1,13 @@
+package javabasics;
+
+public class Hashtag {
+
+    public static void main(String[] args) {
+
+        String hashtag = "#100DaysOfCode";
+
+        // Make the hashtag lowercase:
+
+
+    }
+}
