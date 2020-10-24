cb59a0cb60682a9c6f39f36b8526f03669bb8e23
Qingliao Wu <qingliaowu@gmail.com>
2020-10-24 13:08:07 +0900

    update

---
+++ JavaCode/src/main/java/javabasics/DNA.java
@@ -0,0 +1,21 @@
+package javabasics;
+
+
+public class DNA{
+    // DNA
+    public static void main(String[] args) {
+
+        //  -. .-.   .-. .-.   .
+        //    \   \ /   \   \ /
+        //   / \   \   / \   \
+        //  ~   `-~ `-`   `-~ `-
+
+        String dna1 = "ATGCGATACGCTTGA";
+        String dna2 = "ATGCGATACGTGA";
+        String dna3 = "ATTAATATGTACTGA";
+        String dna = dna1;
+
+        System.out.println(dna.length());
+
+    }
+}
---
+++ JavaCode/src/main/java/javabasics/Noodle.java
@@ -0,0 +1,23 @@
+package javabasics;
+
+class Noodle {
+
+    double lengthInCentimeters;
+    String shape;
+    String texture = "brittle";
+
+    public void cook() {
+
+        this.texture = "cooked";
+
+    }
+
+    public static void main(String[] args) {
+
+        Spaghetti spaghettiPomodoro = new Spaghetti();
+
+        System.out.println(spaghettiPomodoro.texture);
+
+    }
+
+}
