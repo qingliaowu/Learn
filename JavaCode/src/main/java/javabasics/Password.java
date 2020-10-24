9f2742511350c252fdad879f25dc0e61bb2c9c64
Qingliao Wu <qingliaowu@gmail.com>
2020-10-24 10:03:20 +0900

    update

---
+++ JavaCode/src/main/java/javabasics/Password.java
@@ -0,0 +1,16 @@
+package javabasics;
+
+public class Password {
+
+    public static void main(String[] args) {
+
+        String password = "correcthorsebatterystaple";
+
+
+        // Write the code:
+        System.out.println(password.equals("correcthorsebatterystaple"));
+
+
+    }
+
+}
