874a70cff3d712b36a799ff035a10f6d6e9076f6
Qingliao Wu <qingliaowu@gmail.com>
2020-10-24 09:38:40 +0900

    update

---
+++ JavaCode/src/main/java/javabasics/HelloWorld2.java
@@ -0,0 +1,18 @@
+package javabasics;
+
+public class HelloWorld2 {
+
+    public static void main(String[] args) {
+
+        String str = "Hello, World!";
+
+        // Examples:
+
+        System.out.println(str.length());
+
+        System.out.println(str.substring(4));
+
+        System.out.println(str.toUpperCase());
+
+    }
+}
