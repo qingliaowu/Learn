3ef6952a1c2caf5825a9097bbf0b241eefe0734b
Qingliao Wu <qingliaowu@gmail.com>
2020-10-24 11:32:20 +0900

    update

--- JavaCode/src/main/java/javabasics/Initials.java
+++ JavaCode/src/main/java/javabasics/Initials.java
@@ -5,10 +5,12 @@ public class Initials {
     public static void main(String[] args) {
 
         // Add a first name and a last name:
-        String firstName = "";
-        String lastName = "";
+        String firstName = "sdfsdf";
+        String lastName = "sdfdhn";
 
         // What are the initials?
+        System.out.println(firstName.charAt(0));
+        System.out.println(lastName.charAt(0));
 
 
     }
