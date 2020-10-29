a1b5f17a8885e99d75304059e018116b8066a892
Qingliao Wu <qingliaowu@gmail.com>
2020-10-29 23:02:18 +0900

    update

---
+++ JavaCode/src/main/java/javabasics/Dinner.java
@@ -0,0 +1,22 @@
+package javabasics;
+
+class Dinner {
+
+    private void makeNoodles(Noodle noodle, String sauce) {
+
+        noodle.cook();
+
+        System.out.println("Mixing " + noodle.texture + " noodles made from " + noodle.ingredients + " with " + sauce + ".");
+        System.out.println("Dinner is served!");
+
+    }
+
+    public static void main(String[] args) {
+
+        Dinner noodlesDinner = new Dinner();
+        // Add your code here:
+
+
+    }
+
+}
