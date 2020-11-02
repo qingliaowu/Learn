4fe798ff3d0e7e8b9498b52fd4a59076657070f6
Qingliao Wu <qingliaowu@gmail.com>
2020-11-03 08:35:28 +0900

    update

--- JavaCode/src/main/java/javabasics/Language.java
+++ JavaCode/src/main/java/javabasics/Language.java
@@ -1,4 +1,15 @@
 package javabasics;
 
 public class Language {
+
+    protected String name;
+    protected int numSpeakers;
+    protected String regionsSpoken;
+    protected String wordOrder;
+
+
+    public static void main(String[] args){
+
+
+    }
 }
