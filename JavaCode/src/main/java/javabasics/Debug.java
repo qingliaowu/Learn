d49981c2dd38c104dc278b337f36afbe91134582
Qingliao Wu <qingliaowu@gmail.com>
2020-11-04 23:25:08 +0900

    update

---
+++ JavaCode/src/main/java/javabasics/Debug.java
@@ -0,0 +1,14 @@
+package javabasics;
+
+public class Debug {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("       1");
+        System.out.println("     2 3");
+        System.out.println("   4 5 6");
+        System.out.println("7 8 9 10");
+
+    }
+}
