2e2e30318c51b5506b811fe0f585b5e9a42ee09c
Qingliao Wu <qingliaowu@gmail.com>
2020-11-18 16:28:18 +0900

    update

--- DataStructure/src/main/java/trees/Node.java
+++ DataStructure/src/main/java/trees/Node.java
@@ -3,6 +3,53 @@ package trees;
 class Node {
 
     Node left, right;
+    int data;
+    public Node(int data){
+        this.data = data;
+    }
+
+    public void insert(int value){
+        if(value < value){
+            if(left == null) {
+                left = new Node(value);
+            }else {
+                left.insert(value);
+            }
+        }else{
+            if(right == null) {
+                right = new Node(value);
+            }else{
+                right.insert(value);
+            }
+        }
+    }
+    public boolean contains(int value){
+        if(value == data){
+            return true;
+        }else if (value < data){
+            if (left == null){
+                return false;
+            }else{
+                return left.contains(value);
+            }
+        }else{
+            if (right == null){
+                return false;
+            }else {
+                return right.contains(value);
+            }
+        }
+    }
+
+    public void printInOrder(){
+        if(left != null){
+            left.printInOrder();
+        }
+        System.out.println(data);
+        if(right != null){
+            right.printInOrder();
+        }
+    }
 
 
 }
