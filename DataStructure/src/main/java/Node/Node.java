8e68737d76bfc0cbc7c875c0a178b90e239e8de9
Qingliao Wu <qingliaowu@gmail.com>
2020-11-23 22:41:10 +0900

    update

--- DataStructure/src/main/java/Node/Node.java
+++ DataStructure/src/main/java/Node/Node.java
@@ -5,6 +5,11 @@ public class Node {
     public String data;
     public Node next;
 
+    public Node(String data){
+        this.data = data;
+
+    }
+
 
     public static void main(String[] args){
 
