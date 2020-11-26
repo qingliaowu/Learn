f32c51173f2d40daa80e52ac164f18600778401c
Qingliao Wu <qingliaowu@gmail.com>
2020-11-26 13:32:16 +0900

    update

--- DataStructure/src/main/java/Node/Node.java
+++ DataStructure/src/main/java/Node/Node.java
@@ -5,19 +5,21 @@ public class Node {
     public String data;
     public Node next;
 
-    public Node(String data){
+    public Node(String data) {
         this.data = data;
         this.next = null;
+    }
 
+    public void setNextNode(Node node){
+        this.next = node;
     }
 
     public static void main(String[] args) {
-
         Node firstNode = new Node("I am a Node!");
+        Node secondNode = new Node("I am the second Node!");
 
-        System.out.println(firstNode.data);
-        System.out.println(firstNode.next);
-
-
+        firstNode.setNextNode(secondNode);
+        System.out.println(firstNode.next.data);
     }
+
 }
