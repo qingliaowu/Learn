6ac5ea3f95e0e41768352fe7d5243eda44ecf409
Qingliao Wu <qingliaowu@gmail.com>
2020-12-01 00:30:25 +0900

    update

--- DataStructure/src/main/java/Linkedlist/LinkedList.java
+++ DataStructure/src/main/java/Linkedlist/LinkedList.java
@@ -4,18 +4,29 @@ public class LinkedList {
 
     public Node head;
 
-    public LinkedList(){
+    public LinkedList() {
         this.head = null;
     }
 
-    public void addToHead(String data){
-
+    public void addToHead(String data) {
         Node newHead = new Node(data);
         Node currentHead = this.head;
         this.head = newHead;
+        if (currentHead != null) {
+            this.head.setNextNode(currentHead);
+        }
     }
 
-
+    public void addToTail(String data) {
+        Node tail = this.head;
+        if (tail == null) {
+            this.head = new Node(data);
+        } else {
+            while(tail.getNextNode() != null){
+                tail = tail.getNextNode();
+            }
+        }
+    }
 
     public static void main(String []args) {
 
