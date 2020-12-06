5672e18f7046c7939f92cd6e9e917a9dc0c728b4
Qingliao Wu <qingliaowu@gmail.com>
2020-12-06 16:40:01 +0900

    update

--- DataStructure/src/main/java/DoublyLinkedList/DoublyLinkedList.java
+++ DataStructure/src/main/java/DoublyLinkedList/DoublyLinkedList.java
@@ -5,7 +5,29 @@ public class DoublyLinkedList {
     public static void main(String[] args) {
         // Create your subway line here:
 
+        DoublyLinkedList subway = new DoublyLinkedList();
 
+        subway.addToHead("Times Square");
+        subway.addToHead("Grand Central");
+        subway.addToHead("Central Park");
+
+        subway.printList();
+
+        subway.addToTail("Penn Station");
+        subway.addToTail("Wall Street");
+        subway.addToTail("Brooklyn Bridge");
+        subway.printList();
+
+        // subway.removeHead();
+        // subway.removeHead();
+        // subway.removeHead();
+        // subway.addToHead("Times Square");
+        // subway.addToHead("Grand Central");
+        subway.removeByData("Central Park");
+        subway.removeTail();
+        subway.printList();
+        subway.removeByData("Times Square");
+        subway.printList();
 
     }
 
