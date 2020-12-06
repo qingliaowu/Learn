d2a41aa451d10b21294dc25a99e2f63e6cb68bce
Qingliao Wu <qingliaowu@gmail.com>
2020-12-06 20:14:00 +0900

    update

--- DataStructure/src/main/java/DoublyLinkedList/DoublyLinkedList.java
+++ DataStructure/src/main/java/DoublyLinkedList/DoublyLinkedList.java
@@ -5,29 +5,18 @@ public class DoublyLinkedList {
     public static void main(String[] args) {
         // Create your subway line here:
 
-        DoublyLinkedList subway = new DoublyLinkedList();
+        DoublyLinkedList testList = new DoublyLinkedList();
+        testList.addToHead("nine");
+        testList.removeTail();
 
-        subway.addToHead("Times Square");
-        subway.addToHead("Grand Central");
-        subway.addToHead("Central Park");
-
-        subway.printList();
-
-        subway.addToTail("Penn Station");
-        subway.addToTail("Wall Street");
-        subway.addToTail("Brooklyn Bridge");
-        subway.printList();
-
-        // subway.removeHead();
-        // subway.removeHead();
-        // subway.removeHead();
-        // subway.addToHead("Times Square");
-        // subway.addToHead("Grand Central");
-        subway.removeByData("Central Park");
-        subway.removeTail();
-        subway.printList();
-        subway.removeByData("Times Square");
-        subway.printList();
+        testList.addToTail("eight");
+        testList.addToTail("two");
+        testList.removeHead();
+        testList.addToTail("four");
+        testList.removeByData("nine");
+        testList.removeHead();
+        System.out.println(testList.head.data);
+        testList.printList();
 
     }
 
