233bbb76217b90bf08e6c2b38c357065dd77d62f
Qingliao Wu <qingliaowu@gmail.com>
2020-12-08 00:03:37 +0900

    update

---
+++ Algorithms/src/main/java/Search/BinarySearch.java
@@ -0,0 +1,19 @@
+package Search;
+
+public class BinarySearch {
+    public static int search(int[] arr, int target) {
+        // Add left and right variables below
+
+        // Add mid calculation below
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] searchable = {1, 2, 7, 8, 22, 28, 41, 58, 67, 71, 94};
+        int target = 41;
+
+        System.out.println(search(searchable, target));
+    }
+
+}
