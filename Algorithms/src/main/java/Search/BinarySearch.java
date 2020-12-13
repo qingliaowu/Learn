a50227464beeedbbaca7fc7817f996992c7521bf
Qingliao Wu <qingliaowu@gmail.com>
2020-12-13 22:13:25 +0900

    update

--- Algorithms/src/main/java/Search/BinarySearch.java
+++ Algorithms/src/main/java/Search/BinarySearch.java
@@ -2,29 +2,25 @@ package Search;
 
 public class BinarySearch {
     public static int search(int[] arr, int target) {
-        // Add left and right variables below
         int left = 0;
         int right = arr.length;
 
+        while(condition) {
+            int mid = Math.floorDiv(left + right, 2);
+            int midValue = arr[mid];
 
-        // Add mid calculation below
-        int mid = Math.floorDiv(left+right, 2);
+            if (midValue == target) {
+                return mid;
+            } // Add else if and else blocks:
 
-        int midValue = arr[mid];
-
-        if(midValue == target){
-            return mid;
         }
 
-        // 2. Create a conditional below
-
         return -1;
-
     }
 
     public static void main(String[] args) {
         int[] searchable = {1, 2, 7, 8, 22, 28, 41, 58, 67, 71, 94};
-        int target = 41;
+        int target = 28;
 
         System.out.println(search(searchable, target));
     }
