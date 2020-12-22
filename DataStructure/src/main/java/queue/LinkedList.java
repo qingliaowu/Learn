93f7422071ccd711622329f1b4339fe3198860dc
Qingliao Wu <qingliaowu@gmail.com>
2020-12-23 00:19:46 +0900

    update

---
+++ DataStructure/src/main/java/queue/LinkedList.java
@@ -0,0 +1,52 @@
+package queue;
+
+public class LinkedList {
+
+    public Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public void addToHead(String data) {
+        Node newHead = new Node(data);
+        Node currentHead = this.head;
+        this.head = newHead;
+        if (currentHead != null) {
+            this.head.setNextNode(currentHead);
+        }
+    }
+
+    public void addToTail(String data) {
+        Node tail = this.head;
+        if (tail == null) {
+            this.head = new Node(data);
+        } else {
+            while (tail.getNextNode() != null) {
+                tail = tail.getNextNode();
+            }
+            tail.setNextNode(new Node(data));
+        }
+    }
+
+    public String removeHead() {
+        Node removedHead = this.head;
+        if (removedHead == null) {
+            return null;
+        }
+        this.head = removedHead.getNextNode();
+        return removedHead.data;
+    }
+
+    public String toString() {
+        Node currentNode = this.head;
+        String output = "<head> ";
+        while (currentNode != null) {
+            output += currentNode.data + " ";
+            currentNode = currentNode.getNextNode();
+        }
+        output += "<tail>";
+        return output;
+    }
+
+}
