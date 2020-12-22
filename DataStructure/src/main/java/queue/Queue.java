e3403ddbe2a884958db90dcb26b8c9a95206f89a
Qingliao Wu <qingliaowu@gmail.com>
2020-12-23 00:26:35 +0900

    update

--- DataStructure/src/main/java/queue/Queue.java
+++ DataStructure/src/main/java/queue/Queue.java
@@ -1,4 +1,21 @@
 package queue;
 
 public class Queue {
+
+    public LinkedList queue;
+    public int size;
+
+    public Queue() {
+        this.queue = new LinkedList();
+        this.size = 0;
+
+    }
+
+    public static void main(String[] args) {
+
+        Queue newQueue = new Queue();
+        System.out.print("This queue has "+ newQueue.size + " nodes.");
+
+    }
+
 }
