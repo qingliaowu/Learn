1d7d241e19379a1c4fe09b59e9aa6d2018f3389b
Qingliao Wu <qingliaowu@gmail.com>
2020-12-23 23:35:32 +0900

    update

--- DataStructure/src/main/java/queue/Queue.java
+++ DataStructure/src/main/java/queue/Queue.java
@@ -4,18 +4,23 @@ public class Queue {
 
     public LinkedList queue;
     public int size;
+    static final int DEFAULT_MAX_SIZE = Integer.MAX_VALUE;
+    public int maxSize;
 
     public Queue() {
-        this.queue = new LinkedList();
-        this.size = 0;
+        this(DEFAULT_MAX_SIZE);
+    }
+
+    public Queue(int maxSize){
 
     }
 
     public static void main(String[] args) {
-
-        Queue newQueue = new Queue();
-        System.out.print("This queue has "+ newQueue.size + " nodes.");
-
+      /*
+      Queue queueOne = new Queue(10);
+      System.out.println("queueOne can have "+ queueOne.maxSize + " nodes!");
+      Queue queueTwo = new Queue();
+      System.out.println("queueTwo can have "+ queueTwo.maxSize + " nodes!");
+      */
     }
-
 }
